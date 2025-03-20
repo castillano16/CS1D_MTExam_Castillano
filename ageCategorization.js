b2cd5052df8e54254a7c171c3fd8547d71030252
castillano16 <marbelencastillano3@gmail.com>
2025-03-20 20:19:23 +0800

    Create ageCategorization.js

---
+++ ageCategorization.js
@@ -0,0 +1,17 @@
+let age = ("Enter your age");
+
+if (age < 5) {
+console. log ("You are a Toddler") ;
+if (age >= 5;<= 12) ;
+console. log ("You are a Child") ;
+if (age >= 13; <= 19) ;
+console. log ("You are a Teenager") ;
+if (age >= 20; <= 35) ;
+console. log ("You are a Young Adult") ;
+if (age >= 36; <= 60) ;
+console. log ("You are at Middle-Aged") ;
+if (age > 60) ;
+console. log ("You are a Senior") ;
+}
+
+
