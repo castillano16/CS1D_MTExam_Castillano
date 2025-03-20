3e0d4f637c3332be430de4327001fca79b73c958
castillano16 <marbelencastillano3@gmail.com>
2025-03-20 20:40:06 +0800

    Update ageCategorization.js

--- ageCategorization.js
+++ ageCategorization.js
@@ -1,17 +1,17 @@
 let age = ("Enter your age");
 
-if (age < 5) {
+if (age < 5);
 console. log ("You are a Toddler") ;
-if (age >= 5;<= 12) ;
+if (age >= 5 && age <= 12) ;
 console. log ("You are a Child") ;
-if (age >= 13; <= 19) ;
+if (age >= 13 && age <= 19) ;
 console. log ("You are a Teenager") ;
-if (age >= 20; <= 35) ;
+if (age >= 20 && age <= 35) ;
 console. log ("You are a Young Adult") ;
-if (age >= 36; <= 60) ;
+if (age >= 36 && age <= 60) ;
 console. log ("You are at Middle-Aged") ;
 if (age > 60) ;
 console. log ("You are a Senior") ;
-}
+
 
 
