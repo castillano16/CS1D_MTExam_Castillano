fcb70ef2f6015a3d24e357252d21f1d5b1addd56
castillano16 <marbelencastillano3@gmail.com>
2025-03-20 19:59:34 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let FullName = prompt ("Enter your FullName, (firstname and last name));
+let Age = prompt ("Enter your Age ;
+let FavoriteNumber = prompt ("Enter your FavoriteNumber);
+let FavoriteColor = prompt ("Enter your FavoriteColor);
+
+console. log (FullName);
+console. log (Age);
+console. log (FavoriteNumber);
+console. log (FavoriteColor) ;
