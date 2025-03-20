c5cb1df46f9b312600145fc20c13335fcb06bbd0
castillano16 <marbelencastillano3@gmail.com>
2025-03-20 20:10:47 +0800

    Create classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,8 @@
+let SubjectTitle = prompt ("Data Structures & Algorithm");
+let ClassSchedule = prompt ("9:00-10:00 AM");
+let Classroom = prompt("CHED 124") ;
+let ClassInstructor = prompt("Sir Owen Pascua") ;
+let StudentName = prompt("Marbelen Castillano") ;
+
+console. log ("{StudentName} is currently enrolled in {SubjectTitle} with a class schedule {ClassSchedule}
+              at room {Classroom}. The instructor for the subject is {ClassInstructor}."
