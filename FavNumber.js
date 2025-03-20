b25754a577c137b37fd5c0ddd58b14f6b0fbd481
castillano16 <marbelencastillano3@gmail.com>
2025-03-20 20:52:02 +0800

    Update FavNumber.js

--- FavNumber.js
+++ FavNumber.js
@@ -1,1 +1,6 @@
 let FavNumber = prompt ("Enter your FavoriteNumber") ;
+let guess = prompt ( "Guess number between 1 and 10") ;
+console. log (guess > too high) ;
+console. log(guess < too low) ;
+console. log ( guess >= correct) ;
+
