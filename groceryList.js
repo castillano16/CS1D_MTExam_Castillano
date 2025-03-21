5ba3737cc0f89288d797f21b7023259cc3846a52
castillano16 <marbelencastillano3@gmail.com>
2025-03-21 19:22:43 +0800

    Update groceryList.js

--- groceryList.js
+++ groceryList.js
@@ -1,10 +1,16 @@
 let grocery_list = [];
-let grocery_list = prompt("Do you want to create a grocery lost? (y/n)")
+let grocery_list = prompt("Do you want to create a grocery list? (y/n)")
 
 if (grocery_list === "y") {
   let count = prompt(" How many items to get? ");
 
   for (let I = 0; i < count; i++) {
     let item = prompt (`Enter item ${i + 1}:`) 
-    grocery_list.push(item);
-    
+    grocery_list.(item);
+  }
+  
+let grocery_list_sorted = grocery_list.sort() ;
+let grocery_list_reverse = grocery_list.reverse() ;
+} else {
+  alert(" Create a grocery list.");
+}    
